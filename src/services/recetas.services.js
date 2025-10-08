c281e3b3a608205a545e8824ed0d3fb0dd4ebb48
KarolainReyes <reyeskarol0412@gmail.com>
2025-10-08 08:53:59 -0500

    progress: :construction: recipes-service  ongoing development of recipe service
     Continue building the recipe service to manage recipe-related operations.
     This commit expands the initial structure with preliminary logic and
     preparation for database interactions, validations, and CRUD features
     to be completed in future updates.
    
    by@KarolainReyes

---
+++ src/services/recetas.services.js
@@ -0,0 +1,57 @@
+import { obtenerDB } from "../config/db.js";
+
+const COLECCION_RECETAS = "recetas"
+
+export async function obtenerJugadores() {
+    return await obtenerDB().collection(COLECCION_RECETAS).find().toArray();
+}
+
+export async function crearReceta(datos) {
+    const {id, titulo, descripcion, difultad, categoria, ingredientes } = datos
+
+    const receta = {
+        id,
+        titulo,
+        descripcion,
+        difultad,
+        categoria,
+        ingredientes
+    }
+
+    await obtenerDB().collection(COLECCION_RECETAS).insertOne(receta);
+    return { message: "Receta creada!"}
+}
+
+export async function actualizarReceta (id, datos){
+    //crear validador de campos
+}
+
+export async function eliminarReceta(params) {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//- Permitir que un usuario pueda registrar una nueva receta con su título y descripción.
+// - Listar todas las recetas disponibles en la plataforma.
+// - Consultar una receta en específico con todos sus ingredientes.
+// - Editar el título o descripción de una receta.
+// - Eliminar una receta.
+// - **Listar todas las recetas que pertenecen a un usuario específico** (ej. “ver todas las recetas de Juan Pérez”).
