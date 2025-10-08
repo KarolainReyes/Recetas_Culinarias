453e9d1d41900a481ad2c16318504a80bce39100
Karolain Reyes <reyeskarol0412@gmail.com>
2025-10-07 20:29:07 -0500

    feat: :sparkles: db-connection  add MongoDB connection module with dotenv configuration
     Create `db.js` to handle MongoDB connection using environment variables
     from dotenv. The module includes `conectarDB()` to establish the
     connection and `obtenerDB()` to retrieve the database instance.
     This setup ensures a modular, reusable, and secure connection to the DB.
    
    by@KarolainReyes

---
+++ config/db.js
@@ -0,0 +1,25 @@
+import { MongoClient } from "mongodb";
+import 'dotenv/config';
+
+const uri = process.env.MONGO_URI
+const db_name = process.env.DB_NAME
+
+const cliente = new MongoClient (uri);
+let db;
+
+export async function conectarDB() {
+    try {
+        await cliente.connect();
+        console.log("DB conectada!");
+        db = cliente.db(db_name);
+    } catch (error) {
+        console.error("Error al conectar con la DB", error)
+    }
+}
+
+export async function obtenerDB() {
+    if(!db){
+        throw new Error("No se ha conectado con la DB!");
+    }
+    return db;
+}
