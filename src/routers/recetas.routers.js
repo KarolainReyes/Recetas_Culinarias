55502418378f0de6988e45217d20dee234d69d17
Karolain Reyes <reyeskarol0412@gmail.com>
2025-10-09 19:06:58 -0500

    fix: :bug: routers  correct errors in router configuration and structure
     Fix issues within the router files to ensure proper endpoint handling
     and connection with controllers. These corrections improve route
     resolution, middleware flow, and overall API stability.
    
    by@KarolainReyes

--- src/routers/recetas.routers.js
+++ src/routers/recetas.routers.js
@@ -5,10 +5,15 @@ import { obtenerTodasLasRecetas,
     actualizarUnaReceta,
     eliminarUnaReceta
  } from "../controllers/recetas.controllers";
+import { eliminarReceta } from "../services/recetas.services";
 
  const router = Router();
 
  router.get("/", obtenerTodasLasRecetas);
- router.get("/:id", obtenerUnaReceta,
- router.post("/",)
- )
+ router.get("/:id", obtenerUnaReceta);
+ router.post("/", crearUnaReceta);
+ router.patch("/:id", actualizarUnaReceta);
+ router.delete("/:id", eliminarUnaReceta);
+
+ export default router;
+ 
